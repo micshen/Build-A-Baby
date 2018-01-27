fb0c18320c5a3d198e1f70aabf5f76c03d2a6d3a
Michelle Shen <michellecshen@gmail.com>
2018-01-26 22:08:39 -0500

    created BabyProperty

---
+++ Assets/Scripts/Baby/BabyProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Baby.Interfaces;
+using UnityEngine;
+
+namespace Baby
+{
+	public class BabyProperty : MonoBehaviour
+	{
+		public GameObject BabyPropertyGameObject;
+
+		public bool Attached;
+
+		public IOption Option; 
+
+	}
+}
