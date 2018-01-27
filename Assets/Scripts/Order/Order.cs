2b9c4b0b7cbe2aee230309f8e0cb6d10351c2456
Michelle Shen <michellecshen@gmail.com>
2018-01-27 15:42:45 -0500

    started creating order generator and order prefabs

---
+++ Assets/Scripts/Order/Order.cs
@@ -0,0 +1,29 @@
+﻿using Baby;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Order : MonoBehaviour {
+
+    public List<BabyProperty> BabyProperties;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true); 
+    }
+}
---
+++ Assets/Scripts/Order/OrderGenerator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class OrderGenerator : MonoBehaviour
+{
+    private Timer timer;
+
+    public float StartOrderDelay = 0f; 
+    public float TimePerOrder = 60f;
+    public float OrderInterval = 20f; 
+
+    //put orders in child of OrderGenerator Canvas Object
+    public IEnumerable<Order> Orders;
+
+
+    // Use this for initialization
+    void Start()
+    {
+       // timer = gameObject.AddComponent<Timer>();
+        
+        Orders = GetComponentsInChildren<Order>();
+
+        foreach (var order in Orders)
+        {
+            order.Hide(); 
+        }
+
+        StartCoroutine(ManageOrders()); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator ManageOrders()
+    {
+        foreach (var order in Orders)
+        {
+            DisplayOrder(order); 
+            yield return new WaitForSeconds(OrderInterval);
+        }
+    }
+
+    private void DisplayOrder(Order order)
+    {
+        order.Show(); 
+    }
+}
