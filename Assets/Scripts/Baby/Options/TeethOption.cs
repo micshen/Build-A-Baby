cd714046c0f0967a5cdc55183530dc96357aac47
Michelle Shen <michellecshen@gmail.com>
2018-01-28 00:16:19 -0500

    added teeth option

---
+++ Assets/Scripts/Baby/Options/TeethOption.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic; 
+using Baby.Interfaces;
+using Baby;
+
+public class TeethOption : MonoBehaviour, IOption
+{
+    public IList<BabyProperty> TeethTypes; 
+
+    private void Awake()
+    {
+        TeethTypes = LoadBabyProperties("Teeth/");
+    }
+
+    private IList<BabyProperty> LoadBabyProperties(string path)
+    {
+        return Resources.LoadAll<BabyProperty>(path); 
+    }
+
+
+    public BabyProperty GetRandomProperty()
+    {
+        if (TeethTypes.Count > 1)
+            return TeethTypes[Random.Range(0, TeethTypes.Count)];
+        else
+            return TeethTypes[0]; 
+
+    }
+}
